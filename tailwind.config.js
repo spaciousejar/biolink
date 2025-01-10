ad59c25564e6ca71686c2be9ad6ce974d63a31fb
spaciousejar <jadhavvicky752@gmail.com>
2025-01-10 15:37:22 +0530

    Refactor Tailwind CSS configuration and add theme toggle button in index.vue
    - Simplified tailwind.config.js by removing unused font families and plugins, and updated content paths for better compatibility.
    - Introduced a theme toggle button in index.vue to switch between light and dark modes, enhancing user experience.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "media",
-  theme: {
-    fontFamily: {
-      sans: [
-        "Avenir Next",
-        "Roboto",
-        "-apple-system",
-        "BlinkMacSystemFont",
-        '"Segoe UI"',
-        "Ubuntu",
-        '"Helvetica Neue"',
-        "Arial",
-        '"Noto Sans"',
-        "sans-serif",
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
-      ],
-      mono: [
-        "ui-monospace",
-        "SFMono-Regular",
-        "Menlo",
-        "Monaco",
-        "Consolas",
-        "Liberation Mono",
-        "Courier New",
-        "monospace",
-      ],
-    },
-  },
-  plugins: [require("@tailwindcss/forms")],
   content: [
-    `components/**/*.{vue,js}`,
-    `layouts/**/*.vue`,
-    `pages/**/*.vue`,
-    `composables/**/*.{js,ts}`,
-    `plugins/**/*.{js,ts}`,
-    `App.{js,ts,vue}`,
-    `app.{js,ts,vue}`,
+    './pages/**/*.{vue,js,ts,jsx,tsx}',
+    './components/**/*.{vue,js,ts,jsx,tsx}',
   ],
-};
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
